823c9c7fbdcb58b75b003ac60ed83dee2c7eb753
Matthew Stanciu <mattbstanciu@gmail.com>
2019-08-25 21:21:07 -0400

    Fix freakout when no song is playing

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -27,16 +27,25 @@ export default (req, res) => {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${accessToken}`
         }
-      }).then(data => {
-        console.log('Retrieved currently playing song!')
-        const json = JSON.parse(data)
-
-        const title = json['item']['name']
-        const artist = json['item']['artists'][0]['name']
-        const albumCover = json['item']['album']['images'][0]['url']
-
-        res.json({ artist, title, albumCover })
       })
+        .then(data => {
+          console.log('Retrieved currently playing song!')
+          const json = JSON.parse(data)
+
+          const title = json['item']['name']
+          const artist = json['item']['artists'][0]['name']
+          const albumCover = json['item']['album']['images'][0]['url']
+
+          res.json({ artist, title, albumCover })
+        })
+        .catch(err => {
+          res.json({
+            title: 'Not playing',
+            artist: 'Not playing',
+            albumCover:
+              'https://collegian.com/wp-content/uploads/2017/08/spotify-1759471_1280.jpg'
+          })
+        })
     )
   }
 
