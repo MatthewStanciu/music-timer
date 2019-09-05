312034c545a6b239c22bab14a18245f4c07353da
Matthew Stanciu <mattbstanciu@gmail.com>
2019-09-05 08:09:59 -0400

    Add simplified api code from master

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -15,39 +15,33 @@ function refreshToken() {
   })
 }
 
-export default (req, res) => {
-  async function fetchData() {
-    var accessToken = await refreshToken()
+export default async (req, res) => {
+  const accessToken = await refreshToken()
 
-    refreshToken().then(
-      request({
-        url: 'https://api.spotify.com/v1/me/player/currently-playing?market=US',
-        headers: {
-          Accept: `application/json`,
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`
-        }
+  request({
+    url: 'https://api.spotify.com/v1/me/player/currently-playing?market=US',
+    headers: {
+      Accept: `application/json`,
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+    .then(data => {
+      console.log('Retrieved currently playing song!')
+      const json = JSON.parse(data)
+
+      const title = json['item']['name']
+      const artist = json['item']['artists'][0]['name']
+      const albumCover = json['item']['album']['images'][0]['url']
+
+      res.json({ artist, title, albumCover })
+    })
+    .catch(err => {
+      res.json({
+        title: 'Not playing',
+        artist: 'Not playing',
+        albumCover:
+          'https://collegian.com/wp-content/uploads/2017/08/spotify-1759471_1280.jpg'
       })
-        .then(data => {
-          console.log('Retrieved currently playing song!')
-          const json = JSON.parse(data)
-
-          const title = json['item']['name']
-          const artist = json['item']['artists'][0]['name']
-          const albumCover = json['item']['album']['images'][0]['url']
-
-          res.json({ artist, title, albumCover })
-        })
-        .catch(err => {
-          res.json({
-            title: 'Not playing',
-            artist: 'Not playing',
-            albumCover:
-              'https://collegian.com/wp-content/uploads/2017/08/spotify-1759471_1280.jpg'
-          })
-        })
-    )
-  }
-
-  fetchData()
+    })
 }
