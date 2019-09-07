6096c63baca7724ee117b35f6d1fdd955d4344ee
Matthew Stanciu <mattbstanciu@gmail.com>
2019-09-07 15:08:11 -0400

    Automatically redirect to login

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
+import React from 'react'
 import fetch from 'isomorphic-unfetch'
-import querystring from 'query-string'
-import Router from 'next/router'
-import Link from 'next/link'
 
-const redirect_uri = 'http://localhost:3000/auth'
+const LoginPage = () => React.createElement('div')
 
-const LoginPage = ({ client_id, client_secret, login_url }) => (
-  <div>
-    <Link href={login_url}>
-      <a className="no-underline bg-green hover:bg-green-light text-white font-bold py-2 px-4 border-b-4 border-green-dark hover:border-green rounded">
-        LOG IN
-      </a>
-    </Link>
-  </div>
-)
-
-LoginPage.getInitialProps = ({ req }) =>
-  fetch((req ? `http://${req.headers.host}` : '') + '/api/authdetails').then(
-    res => res.json()
+LoginPage.getInitialProps = async ({ req, res }) => {
+  const authDetails = await fetch(
+    (req ? `http://${req.headers.host}` : '') + '/api/authdetails'
   )
+  const data = await authDetails.json()
+
+  res.writeHead(302, {
+    Location: data.login_url
+  })
+  res.end()
+
+  return {}
+}
 
 export default LoginPage
